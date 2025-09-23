75faa2f7056e19ad40d2183d99d8f234d26aa735
Allynall <Skuridinaalina2006@gmail.com>
2025-09-23 21:31:20 +0300

    Add interface

--- tests/test_tvector.cpp
+++ tests/test_tvector.cpp
@@ -478,7 +478,7 @@ TEST(TestTVectorLib, not_find_elems) {
 //    }
 //    return true;
 //}
-TEST(TestTVectorLib, randomize) { //
+TEST(TestTVectorLib, randomize) { 
 
 
     TVector<int> vec1({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 });
