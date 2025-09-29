9f3b7f8470de96637ff9c2d1654d141d68c5c286
Allynall <Skuridinaalina2006@gmail.com>
2025-09-30 00:07:04 +0300

    Add operation with mvector

--- lib_math_vector/math_vector.h
+++ lib_math_vector/math_vector.h
@@ -4,18 +4,17 @@
 template<typename T>
 class MVector : public TVector<T> {
 public:
-	MVector() : TVector<T>() {}
-	MVector(const T*, size_t) : TVector<T>(const T*, size_t) {}
+	MVector() : TVector<T>() {} //++
+	MVector(const T*, size_t) : TVector<T>(const T*, size_t) {} //++
 	MVector(const TVector<T>& other) : TVector<T>(other) {}
 
-	MVector<T> operator+(const MVector<T>& other) const;
-	MVector<T> operator-(const MVector<T>& other) const;
-	MVector<T> operator*(const MVector<T>& other) const;
+	MVector<T>& operator+(const MVector<T>& other); //++
+	MVector<T>& operator-(const MVector<T> other); //++
+	T operator*(const MVector<T>& other);
 
 	MVector<T>& operator*(const T& num);   //++
-	MVector<T>& operator/(const T& num); 
+	MVector<T>& operator/(const T& num);  //++
 
-	//транспонирование
 };
 
 template<typename T>
@@ -38,4 +37,65 @@ MVector<T>& MVector<T>::operator/(const T& num) {
 	}
 
 	return *this;
+}
+template<typename T>
+MVector<T>& MVector<T>::operator+(const MVector<T>& other) {
+	if ((*this)._size != other._size) {
+		throw std::invalid_argument("The dimensions of the vectors should be equal");
+	}
+	for (int i = 0; i < (*this)._capacity; i++) 
+		if ((*this)._states[i] == busy) {
+		for (int j = i; j < other._capacity; j++)
+		{
+			if (other._states[j] == busy) {
+				(*this)._data[i] = (*this)._data[i] + other._data[j];
+				break;
+			}
+		}
+	}
+
+	return *this;
+}
+template<typename T>
+MVector<T>& MVector<T>::operator-(const MVector<T> other) {
+	if ((*this)._size != other._size) {
+		throw std::invalid_argument("The dimensions of the vectors should be equal");
+	}
+	for (int i = 0; i < other._capacity; i++) {
+		if (other._states[i] == busy) {
+			other._data[i] = -(other._data[i]);
+		}
+	}
+	return (*this) + other;
+	/*for (int i = 0; i < (*this)._capacity; i++)
+		if ((*this)._states[i] == busy) {
+			for (int j = i; j < other._capacity; j++)
+			{
+				if (other._states[j] == busy) {
+					(*this)._data[i] = (*this)._data[i] - other._data[j];
+					break;
+				}
+			}
+		}
+
+	return *this;*/
+}
+template<typename T>
+T MVector<T>::operator*(const MVector<T>& other) {
+	T result = T(0);
+	if ((*this)._size != other._size) {
+		throw std::invalid_argument("The dimensions of the vectors should be equal");
+	}
+	for (int i = 0; i < (*this)._capacity; i++)
+		if ((*this)._states[i] == busy) {
+			for (int j = i; j < other._capacity; j++)
+			{
+				if (other._states[j] == busy) {
+					result += (*this)._data[i] * other._data[j];
+					break;
+				}
+			}
+		}
+
+	return result;
 }
--- tests/test_math_vector.cpp
+++ tests/test_math_vector.cpp
@@ -29,7 +29,19 @@ TEST(TestMVectorLib, size_constructor) {
     EXPECT_EQ(14, vec1.size());
     EXPECT_EQ(15, vec1.capacity());
 }
-TEST(TestMVectorLib, mult_with_scalar) {
+TEST(TestMVectorLib, copy_constructor) {
+    // Arrange
+    MVector<int> vec1({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }), vec2(vec1);
+
+    // Assert
+    EXPECT_FALSE(vec2.is_empty());
+    EXPECT_EQ(14, vec2.size());
+    EXPECT_EQ(15, vec2.capacity());
+    EXPECT_EQ(1, vec2.front());
+    EXPECT_EQ(8, vec2[7]);
+    EXPECT_EQ(14, vec2.back());
+}
+TEST(TestMVectorLib, mult_with_scalar_old) {
     // Arrange
     MVector<int> vec1({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 });
 
@@ -41,6 +53,19 @@ TEST(TestMVectorLib, mult_with_scalar) {
     EXPECT_EQ(5, vec1.front());
     EXPECT_EQ(40, vec1[7]);
     EXPECT_EQ(70, vec1.back());
+}
+TEST(TestMVectorLib, mult_with_scalar_new) {
+    // Arrange
+    MVector<int> vec1({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 });
+
+    //Act
+    MVector<int> vec2 = vec1 * 5;
+    // Assert
+    EXPECT_FALSE(vec2.is_empty());
+    EXPECT_EQ(14, vec2.size());
+    EXPECT_EQ(5, vec2.front());
+    EXPECT_EQ(40, vec2[7]);
+    EXPECT_EQ(70, vec2.back());
 }
 TEST(TestMVectorLib, div_with_scalar) {
     // Arrange
@@ -54,4 +79,88 @@ TEST(TestMVectorLib, div_with_scalar) {
     EXPECT_EQ(1, vec1.front());
     EXPECT_EQ(4, vec1[3]);
     EXPECT_EQ(6, vec1.back());
+}
+TEST(TestMVectorLib, Throw_from_add_vector_any_size) {
+    // Arrange
+    MVector<int> vec1({ 2, 4, 6, 8, 10, 12 }), vec2({ 1, 2 });
+
+    // Assert
+    ASSERT_ANY_THROW(vec1 + vec2);
+}
+TEST(TestMVectorLib, add_vector_old) {
+    // Arrange
+    MVector<int> vec1({ 2, 4, 6 }), vec2({ 1, 2, 3});
+
+    //Act
+    vec1 = vec1 + vec2;
+    // Assert
+    EXPECT_FALSE(vec1.is_empty());
+    EXPECT_EQ(3, vec1.size());
+    EXPECT_EQ(3, vec1.front());
+    EXPECT_EQ(6, vec1[1]);
+    EXPECT_EQ(9, vec1.back());
+}
+TEST(TestMVectorLib, add_vector_new) {
+    // Arrange
+    MVector<int> vec1({ 2, 4, 6 }), vec2({ 1, 2, 3 });
+
+    //Act
+    MVector<int> vec3 = vec1 + vec2;
+    // Assert
+    EXPECT_FALSE(vec3.is_empty());
+    EXPECT_EQ(3, vec3.size());
+    EXPECT_EQ(3, vec3.front());
+    EXPECT_EQ(6, vec3[1]);
+    EXPECT_EQ(9, vec3.back());
+}
+TEST(TestMVectorLib, Throw_from_sub_vector_any_size) {
+    // Arrange
+    MVector<int> vec1({ 2, 4, 6, 8, 10, 12 }), vec2({ 1, 2 });
+
+    // Assert
+    ASSERT_ANY_THROW(vec1 - vec2);
+}
+TEST(TestMVectorLib, sub_vector_old) {
+    // Arrange
+    MVector<int> vec1({ 3, 5, 7 }), vec2({ 1, 2, 3 });
+
+    //Act
+    vec1 = vec1 - vec2;
+    // Assert
+    EXPECT_FALSE(vec1.is_empty());
+    EXPECT_EQ(3, vec1.size());
+    EXPECT_EQ(2, vec1.front());
+    EXPECT_EQ(3, vec1[1]);
+    EXPECT_EQ(4, vec1.back());
+}
+TEST(TestMVectorLib, sub_vector_new) {
+    // Arrange
+    MVector<int> vec1({ 3, 5, 7 }), vec2({ 1, 2, 3 });
+
+    //Act
+    MVector<int> vec3 = vec1 - vec2;
+    // Assert
+    EXPECT_FALSE(vec1.is_empty());
+    EXPECT_EQ(3, vec1.size());
+    EXPECT_EQ(2, vec1.front());
+    EXPECT_EQ(3, vec1[1]);
+    EXPECT_EQ(4, vec1.back());
+}
+TEST(TestMVectorLib, Throw_from_mult_vector_any_size) {
+    // Arrange
+    MVector<int> vec1({ 2, 4, 6, 8, 10, 12 }), vec2({ 1, 2 });
+
+    // Assert
+    ASSERT_ANY_THROW(vec1 * vec2);
+}
+TEST(TestMVectorLib, mult_vector_with_vector) {
+    // Arrange
+    MVector<int> vec1({ 3, 5, 7 }), vec2({ 1, 2, 3 });
+
+    //Act
+    int res = vec1 * vec2;
+    // Assert
+    EXPECT_EQ(34, res);
+    EXPECT_EQ(3, vec1.front());
+    EXPECT_EQ(1, vec2.front());
 }
